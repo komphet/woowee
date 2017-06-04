a6790ee03ef6ad14bc7ae10e7c7bd51943308fd0
Jitiwattana Roru <bbos1@windowslive.com>
2017-06-04 21:39:30 +0700

    CHANGE img size to const and resize in productscreen

--- screens/product-screen/ProductScreen.js
+++ screens/product-screen/ProductScreen.js
@@ -10,6 +10,8 @@ import {
 import Colors from '../../constants/Colors';
 import { Button, Grid, Col, Row } from 'react-native-elements';
 
+const IMGSIZE = 175;
+
 export default class ProductScreen extends React.Component {
   static navigationOptions = {
     title: 'FEORA   ',
@@ -28,23 +30,23 @@ export default class ProductScreen extends React.Component {
         <Image source={require('../../assets/images/bg-water.jpg')} style={styles.water} />
         <Row style={{ alignItems: 'center' }}>
           <TouchableOpacity onPress={() => this._goto(1)} >
-            <Image source={require('../../assets/logo/1.png')} style={{ marginBottom: -22 }} />
+            <Image source={require('../../assets/logo/1.png')} style={{ width: IMGSIZE, height: IMGSIZE, marginBottom: -22 }} />
           </TouchableOpacity>
         </Row>
         <Row style={{ justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row' }}>
           <TouchableOpacity onPress={() => this._goto(2)} >
-            <Image source={require('../../assets/logo/2.png')} style={{ margin: -22 }} />
+            <Image source={require('../../assets/logo/2.png')} style={{ width: IMGSIZE, height: IMGSIZE, margin: -22 }} />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this._goto(3)} >
-            <Image source={require('../../assets/logo/3.png')} style={{ margin: -22 }} />
+            <Image source={require('../../assets/logo/3.png')} style={{ width: IMGSIZE, height: IMGSIZE, margin: -22 }} />
           </TouchableOpacity>
         </Row>
         <Row style={{ justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row' }}>
           <TouchableOpacity onPress={() => this._goto(4)} >
-            <Image source={require('../../assets/logo/4.png')}  style={{ margin: -12 }} />
+            <Image source={require('../../assets/logo/4.png')}  style={{ width: IMGSIZE, height: IMGSIZE, margin: -12 }} />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => this._goto(5)} >
-            <Image source={require('../../assets/logo/5.png')}  style={{ margin: -12 }} />
+            <Image source={require('../../assets/logo/5.png')}  style={{ width: IMGSIZE, height: IMGSIZE, margin: -12 }} />
           </TouchableOpacity>
         </Row>
       </Grid>
